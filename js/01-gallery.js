c13dfd18b3eb8f4a2f50173a620b903e77fa6520
Brudja <n0669378587@gmail.com>
2022-09-10 12:12:44 +0300

    на проверку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+
+const elGallery = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems)
+
+elGallery.insertAdjacentHTML("beforeend", markup);
+elGallery.addEventListener("click", ListenerClikGallery);
+
+
+// разметка
+function createMarkup(items){
+return items.map(item => {
+    return`
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>`;
+}).join("");
+};
+// console.log(createMarkup(galleryItems))
+
+// Слушатель
+
+function ListenerClikGallery(event){
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")){
+        return;    
+    };
+    console.log(event.target.dataset.source)
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+instance.show()
+// instance.close()
+
+    };
